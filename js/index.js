6c652897037d0fc7076b0d4a3718f60eccef23f1
Austin Berry <aberry52098@gmail.com>
2023-09-01 11:48:03 -0400

    simplified submit func.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
 'use strict';
 
 function submit(){
-    //Adjective 1
-    let a = document.querySelector("#adjective1").value;
-    document.querySelector("#blank1").innerHTML = a;
-    //Noun 1
-    let b = document.querySelector("#noun1").value;
-    document.querySelector("#blank2").innerHTML = b;
-    //
-    
-    
+    document.querySelector("#blank1").innerHTML = document.querySelector("#one").value;
+    document.querySelector("#blank2").innerHTML = document.querySelector("#two").value;
+    document.querySelector("#blank3").innerHTML = document.querySelector("#three").value;
+    document.querySelector("#blank4").innerHTML = document.querySelector("#four").value;
+    document.querySelector("#blank5").innerHTML = document.querySelector("#five").value;
+    document.querySelector("#blank6").innerHTML = document.querySelector("#six").value;
+    document.querySelector("#blank7").innerHTML = document.querySelector("#seven").value;
+    document.querySelector("#blank8").innerHTML = document.querySelector("#eight").value;
 }
 
 function story(){
-    window.location.href="/story.html";
+    window.location.href="#box";
+}
+
+function reset(){
+    window.location.href="#home";
+    document.querySelector(["#one"]).value = "";
+    document.querySelector("#two").value = "";
+    document.querySelector("#three").value = "";
+    document.querySelector("#four").value = "";
+    document.querySelector("#five").value = "";
+    document.querySelector("#six").value = "";
+    document.querySelector("#seven").value = "";
+    document.querySelector("#eight").value = "";
+
 }
